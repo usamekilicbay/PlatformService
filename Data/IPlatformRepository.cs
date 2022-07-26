92d3a04f1b0780d3ca491e452bf6aa84a07872ff
usamekilicbay <usame.kilicbay@gmail.com>
2022-07-26 17:21:52 +0300

    Add: IPlatformRepository and define method signs

---
+++ Data/IPlatformRepository.cs
@@ -0,0 +1,20 @@
+﻿using PlatformService.Models;
+using System.Linq.Expressions;
+
+namespace PlatformService.Data
+{
+    public interface IPlatformRepository
+    {
+        Task Add(Platform platform);
+        Task AddRange(IEnumerable<Platform> platforms);
+
+        Task<Platform> Get(int id);
+        Task<IEnumerable<Platform>> GetAll();
+        Task<IEnumerable<Platform>> Find(Expression<Func<Platform, bool>> predicate);
+
+        void Remove(Platform platform);
+        void RemoveRange(IEnumerable<Platform> platforms);
+
+        Task<bool> SaveChanges();
+    }
+}
